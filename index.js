7b637c26f3db105ff55ab2a5ff73d959193f0b39
Surapat Mekvanich <titleyugio@gmail.com>
2022-11-28 08:24:51 -0800

    commit tree bc3ba33ddf9dcc5813319ff04b1af9d18fd37f51

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1/parameter2
+divide(2000,100);
+
+const square = (parameter3) => parameter3 ** parameter3
+
+const add = (parameter4,parameter5) => parameter4 + parameter5
